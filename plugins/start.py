fcf725ab27473765031ad8b2a12ceb86499b68e2
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-03-15 22:24:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,4 @@
 
-from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-import humanize
-from helper.database import  insert 
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
