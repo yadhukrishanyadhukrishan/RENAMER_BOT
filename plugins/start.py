07fad784cea02ce8e2fef3df6878672d1dd2cff7
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-03-15 22:21:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -22,15 +22,3 @@ async def start(client,message):
 
 
 
-@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def send_doc(client,message):
-       media = await client.get_messages(message.chat.id,message.message_id)
-       file = media.document or media.video or media.audio 
-       filename = file.file_name
-       filesize = humanize.naturalsize(file.file_size)
-       fileid = file.file_id
-       await message.reply_text(
-       f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
-       ,reply_to_message_id = message.message_id,
-       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
-       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
